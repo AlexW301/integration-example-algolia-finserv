cdb7553bb804915f16bc299e5cffa689ae6e3910
Alex <alexwaller301@gmail.com>
2023-09-20 09:36:51 -0400

    Update algoliaItem.ts

--- src/functions/utils/algoliaItem.ts
+++ src/functions/utils/algoliaItem.ts
@@ -6,7 +6,6 @@ export type AlgoliaItem = Readonly<{
   codename: string;
   name: string;
   elements: Object;
-  managers: any;
   language: string;
   type: string;
   slug: string;
@@ -49,7 +48,6 @@ export const convertToAlgoliaItem =
     collection: item.system.collection,
     name: item.system.name,
     elements: item.elements,
-    managers: item.elements.managers.linkedItems.map((manager: Manager, index: number) => `${manager.elements.full_name.value}${index < item.elements.managers.linkedItems.length - 1 ? ", " : ""}`),
     investmentType: item.elements.type.value[0].name,
     symbol: item.elements.symbol.value,
     language: item.system.language,
