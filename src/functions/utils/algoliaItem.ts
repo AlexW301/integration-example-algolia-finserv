7ea2f476747212c8f9c321400630fb3652b043f6
Alex <alexwaller301@gmail.com>
2023-09-20 09:25:51 -0400

    Update algoliaItem.ts

--- src/functions/utils/algoliaItem.ts
+++ src/functions/utils/algoliaItem.ts
@@ -36,6 +36,13 @@ interface Manager {
   };
 }
 
+interface ContentItemElementsIndexer {
+  // Define the structure of ContentItemElementsIndexer here
+  linkedItems: Manager[]; // Assuming Manager is the correct type
+  // Other properties...
+}
+
+
 export const canConvertToAlgoliaItem = (expectedSlug: string) => (item: IContentItem): boolean =>
   !!item.elements[expectedSlug];
 
@@ -49,7 +56,7 @@ export const convertToAlgoliaItem =
     collection: item.system.collection,
     name: item.system.name,
     elements: item.elements,
-    managers: (item.elements.managers.linkedItems as Manager[]).map((manager: Manager, index: number) => `${manager.elements.full_name.value}${index < item.elements.managers.linkedItems.length - 1 ? ", " : ""}`),
+    managers: ((item.elements.managers as ContentItemElementsIndexer).linkedItems as Manager[]).map((manager: Manager, index: number) => `${manager.elements.full_name.value}${index < item.elements.managers.linkedItems.length - 1 ? ", " : ""}`),
     investmentType: item.elements.type.value[0].name,
     symbol: item.elements.symbol.value,
     language: item.system.language,
